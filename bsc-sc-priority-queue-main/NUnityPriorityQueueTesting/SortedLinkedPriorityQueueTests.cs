d9d657f20e5926cb87e9ff27ad8dfe807736087a
RossG <rosco20000@gmail.com>
2026-03-30 14:25:02 +0100

    NUnit tests added for SortedLinked. Again, tests appear to function correctly.

--- bsc-sc-priority-queue-main/NUnityPriorityQueueTesting/SortedLinkedPriorityQueueTests.cs
+++ bsc-sc-priority-queue-main/NUnityPriorityQueueTesting/SortedLinkedPriorityQueueTests.cs
@@ -8,7 +8,78 @@ using PriorityQueue;
 
 namespace NUnityPriorityQueueTesting
 {
-    internal class SortedLinkedPriorityQueueTests
+    public class SortedLinkedPriorityQueueTests
     {
+        [Test]
+        public void TestNewQueueEmpty()
+        {
+            SortedLinkedPriorityQueue<string> queue = new SortedLinkedPriorityQueue<string>();
+            Assert.That(queue.IsEmpty(), Is.True);
+        }
+
+        [Test]
+        public void TestEmptyQueueThrowPeek()
+        {
+            SortedLinkedPriorityQueue<string> queue = new SortedLinkedPriorityQueue<string>();
+            Assert.Throws<QueueUnderflowException>(() => queue.Peek());
+        }
+
+        [Test]
+        public void TestEmptyQueueThrowDequeue()
+        {
+            SortedLinkedPriorityQueue<string> queue = new SortedLinkedPriorityQueue<string>();
+            Assert.Throws<QueueUnderflowException>(() => queue.Dequeue());
+        }
+
+        [Test]
+        public void TestReturnHighestPriorityItem()
+        {
+            SortedLinkedPriorityQueue<string> queue = new SortedLinkedPriorityQueue<string>();
+
+            queue.Enqueue("Blue", 2);
+            queue.Enqueue("Red", 5);
+            queue.Enqueue("Green", 1);
+
+            Assert.That(queue.Peek(), Is.EqualTo("Red"));
+        }
+
+        [Test]
+        public void TestDequeueHighest()
+        {
+            SortedLinkedPriorityQueue<string> queue = new SortedLinkedPriorityQueue<string>();
+
+            queue.Enqueue("Blue", 2);
+            queue.Enqueue("Red", 5);
+            queue.Enqueue("Green", 1);
+
+            queue.Dequeue();
+
+            Assert.That(queue.Peek(), Is.EqualTo("Blue"));
+        }
+
+        [Test]
+        public void TestOneDequeue()
+        {
+            SortedLinkedPriorityQueue<string> queue = new SortedLinkedPriorityQueue<string>();
+
+            queue.Enqueue("Blue", 2);
+            queue.Dequeue();
+
+            Assert.That(queue.IsEmpty(), Is.True);
+        }
+
+        [Test]
+        public void TestMultipleDequeues()
+        {
+            SortedLinkedPriorityQueue<string> queue = new SortedLinkedPriorityQueue<string>();
+            queue.Enqueue("Blue", 2);
+            queue.Enqueue("Red", 5);
+            queue.Enqueue("Green", 1);
+            Assert.That(queue.Peek(), Is.EqualTo("Red"));
+            queue.Dequeue();
+            Assert.That(queue.Peek(), Is.EqualTo("Blue"));
+            queue.Dequeue();
+            Assert.That(queue.Peek(), Is.EqualTo("Green"));
+        }
     }
 }
